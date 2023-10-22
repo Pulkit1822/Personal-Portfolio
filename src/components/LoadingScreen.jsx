c57545a730c585c2f2657cd3dd1fb5039d548954
Pulkit Kumar Mathur <97748031+Pulkit1822@users.noreply.github.com>
2023-10-22 16:36:43 +0530

    Add files via upload

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,38 @@
+import { useProgress } from "@react-three/drei";
+import { useEffect } from "react";
+
+export const LoadingScreen = (props) => {
+  const { started, setStarted } = props;
+  const { progress, total, loaded, item } = useProgress();
+
+  useEffect(() => {
+    console.log(progress, total, loaded, item);
+    if (progress === 100) {
+      setTimeout(() => {
+        setStarted(true);
+      }, 500);
+    }
+  }, [progress, total, loaded, item]);
+
+  return (
+    <div
+      className={`fixed top-0 left-0 w-full h-full z-50 transition-opacity duration-2000 pointer-events-none
+  flex items-center justify-center bg-indigo-50 
+  ${started ? "opacity-0" : "opacity-100"}`}
+    >
+      <div className="text-4xl md:text-9xl font-bold text-indigo-900 relative">
+        <div
+          className="absolute left-0 top-0 overflow-hidden truncate text-clip transition-all duration-10"
+          style={{
+            width: `${progress}%`,
+            fontFamily: "Calligraphy",
+            paddingLeft: "1rem",
+          }}
+        >
+          Pulkit Mathur
+        </div>
+        <div className="opacity-0">Pulkit Mathur</div>
+      </div>
+    </div>
+  );
+};
